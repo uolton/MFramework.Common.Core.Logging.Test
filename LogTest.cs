9d2dca2bb86f790007a5d48b40f59ea8725ae1c2
uolton <valter.bricca@gmail.com>
2014-11-24 12:36:31 +0100

    Initial baseline
    Initial Baseline

---
+++ LogTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace MFramework.Common.Core.Logging.Test
+{
+    class LogTest:TestUnitBase
+    {
+        [Test]
+        public void LogginTest()
+        {
+            var log = Log.GetLoggerFromContext();
+            log.Info("Writing Info Text");
+
+        }
+    }
+}
---
+++ TestUnitBase.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MFramework.Common.Core.Logging.Test
+{
+    [TestFixture]
+    public abstract class TestUnitBase
+    {
+        protected MockRepository _mockRepository;
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _mockRepository= new MockRepository();
+        }
+    }
+}
